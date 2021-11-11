184d65b29fdc2e28784602aaef0f2c828abaf409
Jonatan Bölenius <boleni@kth.se>
2021-11-11 22:56:21 +0100

    think im gonna do java instead  Please enter the commit message for your changes. Lines starting

--- high-to-low/multiplication.asm
+++ high-to-low/multiplication.asm
@@ -3,10 +3,26 @@
 	num2: .word 10
 
 .text
-.main
-	lw $t0, num1($zero)
-	lw $t1, num2($zero)
+main:
+	li $a0, 3
+	li $a1, 1
 	
-	add $t2, $t0, $t1
+	jal multiply
+	
 	li $v0, 1
-#	la $a0, $t2 doesnt work
+	move $a0, $v0
+	syscall
+	
+
+multiply:
+	move $t0, $0
+	move $t1, $0
+	addi $t1, $t1, $a1
+	move $t2, $0
+	loop:
+		addi $t2, $a1
+		addi $t0, $t0, 1
+		bne $t0, $t1, loop
+	li $v0, $0
+	add $v0, $0, $t2
+	jr $ra
